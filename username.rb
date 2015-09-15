f54583775e477af00b0e8979da0314bf14bfbcfb
Sarad Gyawali <saradgyawali2010@yahoo.com>
2015-09-15 16:18:13 -0700

    updated username

--- username.rb
+++ username.rb
@@ -2,7 +2,16 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  first=first.gsub(/[\W\d]/,"").downcase
+  last=last.gsub(/[\W\d]/,"").downcase
+
+  if !first.empty? && !last.empty
+
+  name = (first[0] + last)
+else
+    nil
+  end 
+
 end
 
 def format_year
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,17 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  "hello"
 end
 
 def scream(message)
-  message
+  message.upcase  + "!"
 end
 
-def first_char
-  "z"
+def first_char(word)
+  word[0].downcase
 end
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(string)
+  string * 3
 end
